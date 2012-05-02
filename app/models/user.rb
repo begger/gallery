ac738ef50b0fa0cc9ee34d83c40dfe89243784e7
Tony Coconate <me@tonycoconate.com>
2012-05-02 12:06:19 -0500

    attr accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :avatar, :avatar_cache, :remove_avatar, :remote_avatar_url
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :avatar, :avatar_cache, :remove_avatar, :remote_avatar_url
   serialize :data
 
   mount_uploader :avatar, AvatarUploader

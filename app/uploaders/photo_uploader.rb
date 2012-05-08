6b2d02b20cb4ec78a772b36b8c546f0b9f4598ee
Tony Coconate <me@tonycoconate.com>
2012-05-08 12:01:32 -0500

    uploader fixed

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -32,7 +32,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumbnail do
-    process :resize_to_limit => [100, 100]
+    process :resize_to_limit => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
